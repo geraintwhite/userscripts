35d97c7ad068d0ac80d348c700eba9e13e3c3fbd
Geraint White <geraint@android.net>
2014-06-05 17:52:18 +0100

    Updated popout player to add button in feed

--- yt_popout.user.js
+++ yt_popout.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Popout YouTube
 // @namespace    http://dvbris.com
-// @version      1.2.1
+// @version      1.3.0
 // @description  Adds a button to watch a popout version of the video
 // @copyright    2014, Geraint White
 // @match        *://*.youtube.com/*
@@ -9,44 +9,43 @@
 // ==/UserScript==
 
 function popout() {
-  var width, height, player
-  width = $('#player').width()
-  height = $('#player').height()
-  player = $('#movie_player')[0]
+  var width = $('#player').width();
+  var height = $('#player').height();
+  var player = $('#movie_player')[0];
   window.open(
     window.location.href.replace('watch?v=','v/') + '?start=' + player.getCurrentTime().toString().split('.')[0] + '&autoplay=1',
     document.title,
     'width=' + width + ',height=' + height
-  )
-  player.stopVideo()
+  );
+  player.stopVideo();
 }
 
-function addButtn() {
-  if ($('#extra-buttons').length === 0) {
-    $('#watch7-sentiment-actions').append(
-      $('<span />')
-        .addClass('yt-uix-button-group')
-        .attr('id', 'extra-buttons')
-        .css({
-          margin: '10px 0',
-          display: 'block'
-        })
-    );
-  }
-  $('#extra-buttons').append(
-    $('<button />')
-      .addClass('yt-uix-button yt-uix-button-text yt-uix-button-size-default')
-      .attr('id', 'popout-button')
-      .text('Open popout')
-      .css('padding', '0 5px')
-      .click(popout)
-  );
+function addBtn(container) {
+  var btnGrp = $('<span />')
+    .addClass('yt-uix-button-group extra-buttons')
+    .css({
+      margin: '10px 0',
+      display: 'block'
+    });
+
+  var popBtn = $('<button />')
+    .addClass('yt-uix-button yt-uix-button-text yt-uix-button-size-default popout-button')
+    .text('Open popout')
+    .css('padding', '0 5px')
+    .click(popout);
+
+  var hasGrp = $(container).find('.extra-buttons');
+  if (!hasGrp.length) { $(container).append(btnGrp.clone()); }
+  $(hasGrp.selector).append(popBtn.clone());
 }
 
 function main() {
   // from http://userscripts.org/scripts/show/153699
   unsafeWindow.yt.pubsub.instance_.subscribe("init-watch", function(){
-    addButtn();
+    addBtn('#watch7-sentiment-actions');
+  });
+  unsafeWindow.yt.pubsub.instance_.subscribe("init-feed", function(){
+    addBtn('.feed-item-container .yt-lockup-content');
   });
 }
 
