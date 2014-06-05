2d86152562f8b1a3c32b7b9fc64f77963d572c64
Geraint White <geraint@android.net>
2014-06-05 20:59:32 +0100

    Fixed button URL in feed for mp3 downloader

--- yt_mp3.user.js
+++ yt_mp3.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name         YouTube to mp3
 // @namespace    http://dvbris.com
-// @version      1.3.0
+// @version      1.3.1
 // @description  Adds a button to convert YouTube videos to mp3 using video2mp3.net
 // @copyright    2014, Geraint White
 // @match        *://*.youtube.com/*
 // @require      http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
 
-function download() {
-  window.open('http://www.video2mp3.net/loading.php?url='+window.location);
+function download(url) {
+  window.open('http://www.video2mp3.net/loading.php?url='+url);
 }
 
 function addBtn(container) {
@@ -23,8 +23,7 @@ function addBtn(container) {
   var dlBtn = $('<button />')
     .addClass('yt-uix-button yt-uix-button-text yt-uix-button-size-default dl-button')
     .text('Convert to mp3')
-    .css('padding', '0 5px')
-    .click(download);
+    .css('padding', '0 5px');
 
   var hasGrp = $(container).find('.extra-buttons');
   if (!hasGrp.length) { $(container).append(btnGrp.clone()); }
@@ -33,12 +32,22 @@ function addBtn(container) {
 
 function main() {
   // from http://userscripts.org/scripts/show/153699
-  unsafeWindow.yt.pubsub.instance_.subscribe("init-watch", function(){
+  unsafeWindow.yt.pubsub.instance_.subscribe("init-watch", function() {
     addBtn('#watch7-sentiment-actions');
   });
-  unsafeWindow.yt.pubsub.instance_.subscribe("init-feed", function(){
+  unsafeWindow.yt.pubsub.instance_.subscribe("init-feed", function() {
     addBtn('.feed-item-container .yt-lockup-content');
   });
 }
 
 main();
+
+$(document).on('click', '.dl-button', function() {
+  var url = $(this).parent().siblings('.yt-lockup-title').children('a').attr('href');
+  console.log(url)
+  if (url == undefined) {
+    download(window.location);
+  } else {
+    download('http://youtube.com'+url);
+  }
+});
