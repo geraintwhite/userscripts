04930fd1a6ca84d9875782467715925104e7bba2
Geraint White <geraint@android.net>
2014-06-05 17:52:04 +0100

    Updated mp3 downloader to add button in feed

--- yt_mp3.user.js
+++ yt_mp3.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube to mp3
 // @namespace    http://dvbris.com
-// @version      1.2.1
+// @version      1.3.0
 // @description  Adds a button to convert YouTube videos to mp3 using video2mp3.net
 // @copyright    2014, Geraint White
 // @match        *://*.youtube.com/*
@@ -12,32 +12,32 @@ function download() {
   window.open('http://www.video2mp3.net/loading.php?url='+window.location);
 }
 
-function addButtn() {
-  if ($('#extra-buttons').length === 0) {
-    $('#watch7-sentiment-actions').append(
-      $('<span />')
-        .addClass('yt-uix-button-group')
-        .attr('id', 'extra-buttons')
-        .css({
-          margin: '10px 0',
-          display: 'block'
-        })
-    );
-  }
-  $('#extra-buttons').append(
-    $('<button />')
-      .addClass('yt-uix-button yt-uix-button-text yt-uix-button-size-default')
-      .attr('id', 'dl-button')
-      .text('Convert to mp3')
-      .css('padding', '0 5px')
-      .click(download)
-  );
+function addBtn(container) {
+  var btnGrp = $('<span />')
+    .addClass('yt-uix-button-group extra-buttons')
+    .css({
+      margin: '10px 0',
+      display: 'block'
+    });
+
+  var dlBtn = $('<button />')
+    .addClass('yt-uix-button yt-uix-button-text yt-uix-button-size-default dl-button')
+    .text('Convert to mp3')
+    .css('padding', '0 5px')
+    .click(download);
+
+  var hasGrp = $(container).find('.extra-buttons');
+  if (!hasGrp.length) { $(container).append(btnGrp.clone()); }
+  $(hasGrp.selector).append(dlBtn.clone());
 }
 
 function main() {
   // from http://userscripts.org/scripts/show/153699
   unsafeWindow.yt.pubsub.instance_.subscribe("init-watch", function(){
-    addButtn();
+    addBtn('#watch7-sentiment-actions');
+  });
+  unsafeWindow.yt.pubsub.instance_.subscribe("init-feed", function(){
+    addBtn('.feed-item-container .yt-lockup-content');
   });
 }
 
