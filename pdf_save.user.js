763b16b5402e3355d906645ae6c805db97204a28
Geraint White <geraintwhite@gmail.com>
2014-06-15 12:28:55 +0100

    Create pdf_save.user.js

---
+++ pdf_save.user.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name       PDFescape ctrl-s save
+// @namespace  http://geraintwhite.co.uk/
+// @version    0.1
+// @description  Allows the use of ctrl-s to save a PDF on PDFescape
+// @match      http://www.pdfescape.com/open/*
+// @copyright  2014, Geraint White
+// @require    http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+
+$(function() {
+    document.addEventListener(
+        'keydown',
+        function(e) {
+            if (e.ctrlKey && e.keyCode === 83) {
+                $('iframe').contents().find('#pdfe_side a:first-child').click();
+                e.preventDefault();
+                return false;
+            }
+        },
+        false
+    );
+
+    $('iframe').addEventListener(
+        'onload',
+        function() {
+            $('iframe').contents()[0].addEventListener(
+                'keydown',
+                function(e) {
+                    if (e.ctrlKey && e.keyCode === 83) {
+                        $('#pdfe_side a:first-child').click();
+                        e.preventDefault();
+                        return false;
+                    }
+                },
+                false
+            );
+        },
+        false
+	);
+});
